f03903cfef71c546cd63b1c539c7a63272fe99d4
Linda Lai <lindalai.615@gmail.com>
2019-01-23 16:37:38 +1100

    Add pseudocode for configMongoose.js file

--- configMongoose.js
+++ configMongoose.js
@@ -1,15 +1,20 @@
-// DOTENV: Reads MONGO_DRIVER URI from .env file
-require('dotenv').load();
+// MONGOOSE CONFIGURATION: Connects to MongoDB database via Mongoose
 const mongoose = require('mongoose');
 
+// DOTENV: Reads MONGO_DRIVER url from .env file
+require('dotenv').load();
 const driver = process.env.MONGO_DRIVER;
 
+// MONGOOSE: Connects to the MongoDB database on localhost:27017
 mongoose.connect(driver, { useNewUrlParser: true });
 
+// MONGOOSE: Connection event - when successfully connected
 mongoose.connection.on('connected', () => {
-    console.log('connected to mongod');
-    console.log('-------------------------------');
+  console.log('connected to mongod');
+  console.log('-------------------------------');
 });
+
+// MONGOOSE: Connection event - when disconnected
 mongoose.connection.on('error', () => {
-    console.log('failed to connect to mongod');
+  console.log('failed to connect to mongod');
 });
--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 // MIDDLEWARE (CONTROLLERS): Routes for authentication, public and private endpoints
 app.use(require('./controllers'));
 
-// SERVER PORT
+// EXPRESS SERVER PORT
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Server is running on port', port);
