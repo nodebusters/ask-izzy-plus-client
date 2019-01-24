c9f66a647b1b616f0a7e0670fe1f6e5b44030f2c
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-01-24 14:17:48 +1100

    added userSchema and extra info in res.data from /getUserData route

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
-// TODO:create an user model based on the info from google profile. 
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    email: String,
+    firstName: String,
+    lastName: String,
+    organisation:{type: mongoose.Schema.Types.ObjectId, ref: 'Organisation'},
+});
+
+module.exports = mongoose.model('User', userSchema)
