8e3e55c1cd87ed456f5c23a2ed609b26b9d92c7e
Aitzu Hu <whitesyy@gmail.com>
2019-01-25 11:44:23 +1100

    mapping site data v1

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -41,6 +41,7 @@ class Dashboard extends Component {
         // const token = localStorage.getItem('token')
             axios.get(url,options)
                 .then(res => {
+                    console.log(res.data);
                     this.setState({
                             organisation: res.data.organisation, 
                             user: res.data.user
@@ -55,29 +56,40 @@ class Dashboard extends Component {
     componentDidMount(){
         this.handleProtectedRequest();
     }
+
+    sitesDetails = () => {
+        const { organisation } = this.state;
+        console.log(organisation.sitesInOrganisation)
+        let i = 0
+        for (i; i < organisation.sitesInOrganisation.length; i++) {
+            // console.log(organisation.sitesInOrganisation)
+                Object.entries(organisation.sitesInOrganisation[i]).map(([key, value]) => {
+                // if  (key!=='servicesInSite'){
+                //     return <p key={key}>{key}: {value}</p>
+                // }
+                return <p key={key}>{key}: {value}</p>
+                // console.log(key, value)
+            })};
+        }
+    };
     
     render() {
         const { organisation, user } = this.state;
         console.log(organisation);
         console.log(user);
+
+        const sitesDetails = this.sitesDetails();
+        console.log(sitesDetails)
         
         const organisationDetails = Object.entries(organisation).map(([key, value]) => {
             if  (key!=='sitesInOrganisation'){
                 return <p key={key}>{key}: {value}</p>
             }
         });
-
-        const sitesDetails = (
-            <React.Fragment>
-                <p>Name: {organisation.sitesInOrganisation[0].name}</p>
-            </React.Fragment>
-        );
-
-        const servicesDetails = (
-            <React.Fragment>
-                <p>Name: {organisation.sitesInOrganisation[0].servicesInSite[0].name}</p>
-            </React.Fragment>
-        );
+            
+        const servicesDetails = Object.entries(organisation.sitesInOrganisation[0].servicesInSite[0]).map(([key, value]) => {
+                return <p key={key}>{key}: {value}</p>
+        });
        
         const userDetails = Object.entries(user).map(([key, value]) => {
             return <p key={key}>{key}: {value}</p>
