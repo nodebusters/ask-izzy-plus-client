38ea7b9a1e82740dd971bce3bba540c4b32229a5
Linda Lai <lindalai.615@gmail.com>
2019-01-26 15:24:10 +1100

    Fix indentation for Users.js

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class User extends Component {
-    render() {
-        const { user } = this.props
-        return (
-            <React.Fragment>
-                {Object.entries(user).map(([key, value]) => {
-                    return <p key={key}>{key}: {value}</p>
-                })}
-            </React.Fragment>
-        );
-    }
+  render() {
+    const { user } = this.props;
+    return (
+      <React.Fragment>
+        {Object.entries(user).map(([key, value]) => {
+          return (
+            <p key={key}>
+              {key}: {value}
+            </p>
+          );
+        })}
+      </React.Fragment>
+    );
+  }
 }
 export default User;
