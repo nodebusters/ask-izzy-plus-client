8f63d93631febde89d9f954d0e668d9d7d660206
Linda Lai <lindalai.615@gmail.com>
2019-01-26 15:22:16 +1100

    Fix indentation for Organisation.js

--- src/Components/Organisation.js
+++ src/Components/Organisation.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Organisation extends Component {
-    render() {
-        const {organisation} = this.props;
-        return (
-            <React.Fragment>
-                {Object.entries(organisation).map(([key, value]) => {
-                    if (key !== 'sitesInOrganisation') {
-                        return <p key={key}>{key}: {value}</p>
-                    }
-                })}
-
-            </React.Fragment>
-        );
-    }
+  render() {
+    const { organisation } = this.props;
+    return (
+      <React.Fragment>
+        {Object.entries(organisation).map(([key, value]) => {
+          if (key !== "sitesInOrganisation") {
+            return (
+              <p key={key}>
+                {key}: {value}
+              </p>
+            );
+          }
+        })}
+      </React.Fragment>
+    );
+  }
 }
 export default Organisation;
