336d441da53782e312ba33a4d3f086e38bce8908
Linda Lai <lindalai.615@gmail.com>
2019-01-26 15:24:39 +1100

    Fix indentation for App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { Router, Route, Link } from 'react-router-dom';
-import './stylesheets/App.css';
-import history from './history';
-import LogIn from './Components/LogIn';
-import Dashboard from './Components/Dashboard';
+import React, { Component } from "react";
+import { Router, Route, Link } from "react-router-dom";
+import "./stylesheets/App.css";
+import history from "./history";
+import LogIn from "./Components/LogIn";
+import Dashboard from "./Components/Dashboard";
 
 class App extends Component {
   render() {
@@ -11,14 +11,17 @@ class App extends Component {
       <Router history={history}>
         <React.Fragment>
           <nav>
-              <LogIn/>
-            <li className="navigation-links"><Link to="/">Home</Link> </li>
-            <li className="navigation-links"><Link to="/logout">Logout</Link></li>
+            <LogIn />
+            <li className="navigation-links">
+              <Link to="/">Home</Link>{" "}
+            </li>
+            <li className="navigation-links">
+              <Link to="/logout">Logout</Link>
+            </li>
           </nav>
-          
+
           <Route exact path="/login" component={LogIn} />
           <Route exact path="/dashboard" component={Dashboard} />
-
         </React.Fragment>
       </Router>
     );
