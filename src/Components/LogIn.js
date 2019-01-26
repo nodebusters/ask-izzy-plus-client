92e91144d451e7a1999358702dd5f467fab25685
Linda Lai <lindalai.615@gmail.com>
2019-01-26 15:21:34 +1100

    Fix indentation for LogIn.js

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import GoogleLogin from 'react-google-login';
+import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
 // import axios from 'axios';
-import history from '../history'
+import history from "../history";
 
 class LogIn extends Component {
-    responseGoogle = (response) => {
-        // console.log(response.tokenId);
-        localStorage.setItem('token', response.tokenId);
-        history.push('/dashboard')
-    }
+  responseGoogle = response => {
+    // console.log(response.tokenId);
+    localStorage.setItem("token", response.tokenId);
+    history.push("/dashboard");
+  };
 
-    errorGoogle = (response) => {
-        console.log(response);
-    }
+  errorGoogle = response => {
+    console.log(response);
+  };
 
-    render() {
-        const googleClient = process.env.REACT_APP_GOOGLE_CLIENT_ID;        
-        return (
-            <React.Fragment>
-                <GoogleLogin
-                    clientId={googleClient}
-                    buttonText="Login"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.errorGoogle}
-                />
-            </React.Fragment>
-        );
-    }
+  render() {
+    const googleClient = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+    return (
+      <React.Fragment>
+        <GoogleLogin
+          clientId={googleClient}
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.errorGoogle}
+        />
+      </React.Fragment>
+    );
+  }
 }
 export default LogIn;
