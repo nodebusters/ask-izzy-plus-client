336b779c013a6d2d3a45e1011b432503440036d7
Linda Lai <lindalai.615@gmail.com>
2019-01-26 15:23:51 +1100

    Fix indentation for Sites.js

--- src/Components/Sites.js
+++ src/Components/Sites.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class Sites extends Component {
-    render() {
-        const {organisation} = this.props;
-        
-        const { sitesInOrganisation } = organisation;
-        //  console.log('sitesInOrganisation', ': ', sitesInOrganisation);
-        const sites = sitesInOrganisation[0];
-        //  console.log('sites', ': ', sites);
-    
-        return (
-            <React.Fragment>
-                {Object.entries(sites).map(([key, value]) => {
-                    if (key !== 'servicesInSite' && key !== 'openingHours') {
-                        return <p key={key}>{key}: {value} </p>
-                    }
-                })}
+  render() {
+    const { organisation } = this.props;
 
-            </React.Fragment>
-        );
-    }
+    const { sitesInOrganisation } = organisation;
+    //  console.log('sitesInOrganisation', ': ', sitesInOrganisation);
+    const sites = sitesInOrganisation[0];
+    //  console.log('sites', ': ', sites);
+
+    return (
+      <React.Fragment>
+        {Object.entries(sites).map(([key, value]) => {
+          if (key !== "servicesInSite" && key !== "openingHours") {
+            return (
+              <p key={key}>
+                {key}: {value}{" "}
+              </p>
+            );
+          }
+        })}
+      </React.Fragment>
+    );
+  }
 }
 export default Sites;
-
