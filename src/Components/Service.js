28be2a260b5d7ebea0b05659fb7c3773411ff562
Linda Lai <lindalai.615@gmail.com>
2019-01-27 23:29:39 +1100

    Minor pseudocode tweaks

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -8,6 +8,7 @@ class Service extends Component {
     const { service } = this.props;
     return (
       <React.Fragment>
+      {/* Object.entries() returns an array of a given object's own enumerable property [key, value] pairs, in the same order as that provided */}
         {Object.entries(service).map(([key, value]) => {
           return <p key={key}>{key}: {value} </p>
         })}
