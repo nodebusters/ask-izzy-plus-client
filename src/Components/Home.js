d2fd6e62af99184544633c342357a29ffeddf853
Linda Lai <lindalai.615@gmail.com>
2019-01-30 15:04:51 +1100

    Add new code for Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,75 +1,36 @@
 import React, { Component } from 'react';
-import header from "../img/header.png";
-import logo from '../img/askizzy-logo.png'
-import "../stylesheets/Home.css";
+import "./Home.css";
+// import "./Home-2.css";
+import Navigation from './Navigation';
+import Login from './Login-2';
+import Footer from './Footer';
 
 class Home extends Component {
   state = {
-    value: "Hello World"
   }
   handleGoogleLogin = () => {
     console.log('Successfully clicked!')
   }
   render() {
     return (
-      // CONTAINER: ENTIRE PAGE (DODGERBLUE)
+      // CONTAINER: ENTIRE PAGE (DODGERBLUE)gi
       <div className="homepage-container">
-
+        <Navigation />
+      
         {/* CONTAINER: MAIN */}
         <div className="body-container">
 
           {/* HEADER: NAVIGATION BAR/HERO IMAGE */}
-          <div className="header-wrapper">
-            <div className="header-bar">
-              <span className="quick-exit-left">
-              </span>
-              <span className="quick-exit-right">
-                <a href="logout">Logout</a>
-              </span>
-            </div>
-            <div className="image-wrapper">
-              <div className="hero-image">
-                <img src={header} />
-              </div>
-              {/* <div className="logo-image">
-               <img src={logo} />
-              </div> */}
-            </div>
-          </div>
           
-          {/* HEADER: */}
+          {/* HEADER: ASK IZZY */}
           <h1>Welcome to App</h1>
 
-          {/* LOGIN VIA GOOGLE OAUTH */}
-          <div className="login-button-container">
-            <button className="login-button" onClick={this.handleGoogleLogin}>Login</button>
-            <div className="forgotten-password">
-              <a href="forgot-password">Forgot Password?</a>
-            </div>
-          </div>
-        
-        {/* CONTAINER: MAIN (Closing <div> for body-container) */}
-        </div>
+          {/* ASK IZZY PLUS: LOGIN VIA GOOGLE OAUTH */}
+          <Login />
 
-        {/* FOOTER */}
-        <footer className="footer-container">
-          <ul className="footer-list">
-            <li>
-              <a href="about">About</a>
-            </li>
-            <li>
-              <a href="terms">Terms</a>
-            </li>
-            <li>
-              <a href="contact">Contact</a>
-            </li>
-            <li>
-              <a href="resources">Online Resources</a>
-            </li>
-          </ul>
-        </footer>
-      
-      {/* CONTAINER: ENTIRE PAGE (Closing <div> for homepage-container) */}
+        </div>
+        
+        <Footer />
       </div>
     );
   }
