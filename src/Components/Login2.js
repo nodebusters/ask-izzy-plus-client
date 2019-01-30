c5e1b2da34599535cb8bde4082a3997fe08603cc
Linda Lai <lindalai.615@gmail.com>
2019-01-30 16:35:45 +1100

    Replace placeholder Login button with Google OAuth button, styling tweaks to follow

--- src/Components/Login2.js
+++ src/Components/Login2.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
+import GoogleLogin from "react-google-login";
+import history from "../history";
 
 class Login extends Component {
-  state = {
-  }
+  responseGoogle = response => {
+    localStorage.setItem("token", response.tokenId);
+    history.push("/dashboard");
+  };
+
+  errorGoogle = response => {
+    console.log(response);
+  };
+
   handleGoogleLogin = () => {
-    console.log('Successfully clicked!')
-  }
+    console.log("Successfully clicked!");
+  };
   render() {
+    const googleClient = process.env.REACT_APP_GOOGLE_CLIENT_ID;
     return (
       <div className="login-button-container">
-        <button className="login-button" onClick={this.handleGoogleLogin}>Login</button>
+        {/* <button className="login-button" onClick={this.handleGoogleLogin}>
+          Login
+        </button>
+        <div className="forgotten-password">
+          <a href="forgot-password">Forgot Password?</a>
+        </div> */}
+
+        <GoogleLogin
+          clientId={googleClient}
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.errorGoogle}
+          className="login-button"
+        />
         <div className="forgotten-password">
           <a href="forgot-password">Forgot Password?</a>
         </div>
