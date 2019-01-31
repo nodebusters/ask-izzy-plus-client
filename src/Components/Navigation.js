c8c10054ae4f2c4ccda5a1f8fcaf6d7f7510e2df
Linda Lai <lindalai.615@gmail.com>
2019-01-31 22:23:35 +1100

    Implement random hero image for all banner assets

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,32 +1,52 @@
 import React, { Component } from "react";
-// import header from '../Images/banners/food.png';
-import food from '../Images/banners/food.png';
+
+// ASK IZZY PLUS BANNER IMAGES
 import advocacy from '../Images/banners/advocacy.png';
+import atsi from '../Images/banners/atsi.png';
+import atsi2 from '../Images/banners/atsi2.png';
+import centrelink from '../Images/banners/centrelink.png';
+import drugsAlcohol from '../Images/banners/drugs-alcohol.png';
+import everydayThings from '../Images/banners/everyday-things.png';
+import facilities from '../Images/banners/facilities.png';
+import familyViolence from '../Images/banners/family-violence.png';
+import findingWork from '../Images/banners/finding-work.png';
+import food from '../Images/banners/food.png';
+import health from '../Images/banners/health.png';
+import homepage from '../Images/banners/homepage.png';
+import housing from '../Images/banners/housing.png';
+import legal from '../Images/banners/legal.png';
+import lifeSkillsEducation from '../Images/banners/life-skills-education.png';
+import moneyHelp from '../Images/banners/money-help.png';
+import somethingToDo from '../Images/banners/something-to-do.png';
+import supportCounselling from '../Images/banners/support-counselling.png';
+import technology from '../Images/banners/technology.png';
+
+// ASK IZZY PLUS LOGO
 import logo from '../Images/logo/ask-izzy-logo-edited.svg'
 // import logo from './ask-izzy-logo.png'
 // import logo from './ask-izzy-logo-original.svg'
+
+// COMPONENTS
 import LogOut from './LogOut';
 
 class Navigation extends Component {
   render() {
     const { originPage } = this.props;
-    // const randomHeaderImages = ["advocacy", "atsi", "atsi2", "centerlink", "drugs-alcohol", "everyday-things", "facilities", "family-violence", "finding-work", "food", "health", "homepage", "housing", "legal", "life-skills-education", "money-help", "something-to-do", "support-counselling", "technology"]
-    const randomHeaderImages = [food, advocacy]
+    const randomHeaderImages = [advocacy, atsi, atsi2, centrelink, drugsAlcohol, everydayThings, facilities, familyViolence, findingWork, food, health, homepage, housing, legal, lifeSkillsEducation, moneyHelp, somethingToDo, supportCounselling, technology]
     const header = randomHeaderImages[Math.floor(Math.random() * randomHeaderImages.length)];
-    // console.log(randomHeader)
       return (
         <div className="header-wrapper">
-        <div className="header-bar">
-        { originPage === 'dashboard' && 
-          <span className="quick-exit-right">
-            <LogOut />
-          </span>
-        }
-        { originPage === 'home' && 
-          <span className="quick-exit-right">
-            <a href="other-link">Other</a>
-          </span>
-        }
+          <div className="header-bar">
+          { originPage === 'dashboard' && 
+            <span className="quick-exit-right">
+              <LogOut />
+            </span>
+          }
+          { originPage === 'home' && 
+            <span className="quick-exit-right">
+              <a href="other-link">Other</a>
+            </span>
+          }
         </div>
 
         <div className="image-wrapper">
