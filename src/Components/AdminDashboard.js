c24e97a47760c8aacbdd6dc90b89a836a7e24794
Aitzu Hu <whitesyy@gmail.com>
2019-01-31 11:14:00 +1100

    connected backend and frontend with simple msg

---
+++ src/Components/AdminDashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AdminDashboard extends Component {
+  componentDidMount() {
+    console.log("AdminDashboard Component did mount");
+    // this.getUserData();
+  }
+
+  render() {
+      return (
+        <React.Fragment>
+         
+        </React.Fragment>
+      );
+    }
+  }
+export default AdminDashboard;
