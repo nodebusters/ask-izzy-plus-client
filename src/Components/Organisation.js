0294b5091ca45eeee7e5ace29b8978e80d0a4a35
Linda Lai <lindalai.615@gmail.com>
2019-02-05 02:18:58 +1100

    Rename Forms.css to Organisation.css

--- src/Components/Organisation.js
+++ src/Components/Organisation.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import "../stylesheets/Forms.css";
+import "../stylesheets/Organisation.css";
 
 class Organisation extends Component {
   //Declaring state.
