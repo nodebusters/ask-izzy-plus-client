652653841e6275c3c7cb65c23fe37f4a2478d8f2
Linda Lai <lindalai.615@gmail.com>
2019-02-04 23:23:38 +1100

    Add user profile card to User component

--- src/Components/AdminDashboard.js
+++ src/Components/AdminDashboard.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import axios from "axios";
 
 // COMPONENTS
 // import User from './User';
 // import Organisation from './Organisation';
-import Navigation from './Navigation';
-import Footer from './Footer';
-import AdminLogOut from './AdminLogOut';
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import AdminLogOut from "./AdminLogOut";
 
-import '../stylesheets/AdminDashboard.css';
-const jwtDecode = require('jwt-decode');
+import "../stylesheets/AdminDashboard.css";
+const jwtDecode = require("jwt-decode");
 
 class AdminDashboard extends Component {
   state = {
     adminUser: {},
     newUser: {
       email: "",
-      organisation:""
+      organisation: ""
     },
     organisations: [],
     users: []
-  }
+  };
 
   componentDidMount() {
     console.log("AdminDashboard Component did mount");
@@ -30,13 +30,13 @@ class AdminDashboard extends Component {
     this.getAllUsersData();
   }
 
-  getAdminUserData = (e) => {
-    const token = localStorage.getItem('token');
+  getAdminUserData = e => {
+    const token = localStorage.getItem("token");
     const decoded = jwtDecode(token);
-    const {given_name,family_name} = decoded;
+    const { given_name, family_name } = decoded;
     const adminName = given_name;
     const adminLastName = family_name;
-    this.setState({adminName,adminLastName})
+    this.setState({ adminName, adminLastName });
 
     if (token) {
       // console.log(token);
@@ -45,101 +45,96 @@ class AdminDashboard extends Component {
       // Sending the token in the headers to the server:
       const options = {
         headers: { token }
-      }
+      };
       // The server will send back res.data containing { user } and { organisation } objects
-      axios.get(url, options)
-        .then(res => { 
-          // console.log(res.data);
-          const { adminUser, message } = res.data; 
-          this.setState({ adminUser, message });
-        })
+      axios.get(url, options).then(res => {
+        // console.log(res.data);
+        const { adminUser, message } = res.data;
+        this.setState({ adminUser, message });
+      });
     } else {
       console.log("doesnt exists");
-      this.setState({ error: "token error" })
+      this.setState({ error: "token error" });
     }
-  }
+  };
 
-  getOrganisationData = (e) => {
+  getOrganisationData = e => {
     const baseURL = process.env.REACT_APP_BASE_URL;
-    const url = `${baseURL}/protected/organisations`; 
-    const token = localStorage.getItem('token');
+    const url = `${baseURL}/protected/organisations`;
+    const token = localStorage.getItem("token");
     const config = {
-      headers:{
+      headers: {
         token
       }
-    }
-    axios.get(url, config)
-      .then(res => {
-        const organisations = [];
-        for (let i=0; i<res.data.length; i++) {
-          const organisation = {name: "", _id: ""};
-          organisation.name = res.data[i].name
-          organisation._id = res.data[i]._id
-          organisations.push(organisation)
-        }
-        this.setState({ organisations })
-      })
-  }
+    };
+    axios.get(url, config).then(res => {
+      const organisations = [];
+      for (let i = 0; i < res.data.length; i++) {
+        const organisation = { name: "", _id: "" };
+        organisation.name = res.data[i].name;
+        organisation._id = res.data[i]._id;
+        organisations.push(organisation);
+      }
+      this.setState({ organisations });
+    });
+  };
 
   getAllUsersData = () => {
     const baseURL = process.env.REACT_APP_BASE_URL;
-    const url = `${baseURL}/protected/users`; 
-    const token = localStorage.getItem('token');
+    const url = `${baseURL}/protected/users`;
+    const token = localStorage.getItem("token");
     const config = {
-      headers:{
+      headers: {
         token
       }
-    }
-    axios.get(url, config)
-      .then(res => {
-        const users = res.data
-        this.setState({ users })
-  })
-}
+    };
+    axios.get(url, config).then(res => {
+      const users = res.data;
+      this.setState({ users });
+    });
+  };
 
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     const { value, id } = e.currentTarget;
     const { newUser } = this.state;
-    newUser[id]= value; 
+    newUser[id] = value;
     this.setState({ newUser });
-  }
+  };
 
-  deleteOneUser = (e) => {
+  deleteOneUser = e => {
     e.preventDefault();
     console.log("Delete one user request triggered.");
-    console.log(e.currentTarget.id)
-    const user_id = e.currentTarget.id
+    console.log(e.currentTarget.id);
+    const user_id = e.currentTarget.id;
     const baseURL = process.env.REACT_APP_BASE_URL;
     const url = `${baseURL}/protected/user/${user_id}`;
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const config = {
-      headers:{
+      headers: {
         token
       }
-    }
-    axios.delete(url, config)
-    .then(res => this.updateUser(res.data))
-  }
+    };
+    axios.delete(url, config).then(res => this.updateUser(res.data));
+  };
 
-  submitForm = (e) => {
+  submitForm = e => {
     e.preventDefault();
     const baseURL = process.env.REACT_APP_BASE_URL;
-    const url = `${baseURL}/protected/user`; 
+    const url = `${baseURL}/protected/user`;
     const { newUser } = this.state;
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const config = {
-      headers:{
+      headers: {
         token
       }
-    }
+    };
 
-    axios.post(url, newUser, config)
-      .then(res => this.updateUser(res.data))
-  }
+    axios.post(url, newUser, config).then(res => this.updateUser(res.data));
+  };
 
-  updateUser = (newData) => {
-    this.setState({ users: newData })
-  }
+  updateUser = newData => {
+    this.setState({ users: newData });
+  };
 
   render() {
     const { adminUser, users } = this.state;
@@ -153,17 +148,15 @@ class AdminDashboard extends Component {
             <AdminLogOut />
           </nav> */}
 
-          <Navigation originPage='dashboard'/>
+          <Navigation originPage="dashboard" />
 
           <div className="body-container">
-
             <Tabs
               defaultIndex={2}
               onSelect={index => {
-              // console.log(index + 1)
+                // console.log(index + 1)
               }}
             >
-
               <TabList>
                 <Tab>Admin Profile</Tab>
                 <Tab>Add New Users</Tab>
@@ -174,9 +167,9 @@ class AdminDashboard extends Component {
               <TabPanel>
                 <div className="admin-container">
                   <h3>You are now logged in as: </h3>
-                    <p>email: {email}</p>
-                    <p>First Name: {adminName}</p>
-                    <p>Last Name: {adminLastName}</p>
+                  <p>email: {email}</p>
+                  <p>First Name: {adminName}</p>
+                  <p>Last Name: {adminLastName}</p>
                 </div>
               </TabPanel>
 
@@ -185,35 +178,60 @@ class AdminDashboard extends Component {
                   <form id="link_user_organisation" className="add-user-form">
                     <h3>Add New User</h3>
                     <label> New User Email: </label>
-                      <input type="text" id="email" onChange={this.handleInputChange}></input>
-                      <br></br>
+                    <input
+                      type="text"
+                      id="email"
+                      onChange={this.handleInputChange}
+                    />
+                    <br />
                     <label> New User Organisation: </label>
-                      <select id="organisation" onChange={this.handleInputChange}>
-                      <option key="" value="1234567890">Testing Organisation Name</option>
+                    <select id="organisation" onChange={this.handleInputChange}>
+                      <option key="" value="1234567890">
+                        Testing Organisation Name
+                      </option>
                       {organisations.map(organisation => {
-                        return <option key={organisation._id} value={organisation._id}>{organisation.name}</option>
-                      })
-                    }
-                      </select>
-                      <br></br>
-                      <button onClick={this.submitForm} className="create-user-button">Submit</button>
+                        return (
+                          <option
+                            key={organisation._id}
+                            value={organisation._id}
+                          >
+                            {organisation.name}
+                          </option>
+                        );
+                      })}
+                    </select>
+                    <br />
+                    <button
+                      onClick={this.submitForm}
+                      className="create-user-button"
+                    >
+                      Submit
+                    </button>
                   </form>
                 </div>
               </TabPanel>
-              
+
               <TabPanel>
                 <div className="all-users-container">
                   <h3>All users</h3>
                   {users.map(user => {
                     return (
-                      <React.Fragment key={`fragment of`+ user._id}>
-                        <span key={`info of`+user._id}>{user.email} | {user.organisation}</span>
-                        <button key={user._id} id={user._id} onClick={this.deleteOneUser}>Delete</button>
-                        <br></br>
+                      <React.Fragment key={`fragment of` + user._id}>
+                        <span key={`info of` + user._id}>
+                          {user.email} | {user.organisation}
+                        </span>
+                        <button
+                          key={user._id}
+                          id={user._id}
+                          onClick={this.deleteOneUser}
+                        >
+                          Delete
+                        </button>
+                        <br />
                       </React.Fragment>
-                    )
+                    );
                   })}
-                  </div>
+                </div>
               </TabPanel>
 
               <TabPanel>
@@ -221,8 +239,8 @@ class AdminDashboard extends Component {
               </TabPanel>
             </Tabs>
           </div>
-          <Footer/>
-      </div>
+          <Footer />
+        </div>
       );
     } else {
       return (
@@ -234,6 +252,7 @@ class AdminDashboard extends Component {
         </React.Fragment>
       );
     }
-    }
   }
+}
+
 export default AdminDashboard;
--- src/Components/Organisation.js
+++ src/Components/Organisation.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import '../stylesheets/Forms.css';
+import "../stylesheets/Forms.css";
 
 class Organisation extends Component {
   //Declaring state.
@@ -8,67 +8,71 @@ class Organisation extends Component {
     data: {},
     formClass: "readMode",
     editButton: "editButton"
-  }
+  };
 
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     const { value, id } = e.currentTarget;
     const data = this.state.data;
     data[id] = value;
     this.setState({ data });
-  }
+  };
 
-  submitForm = (e) => {
+  submitForm = e => {
     const { updateOrganisation } = this.props;
     e.preventDefault();
     //PUT request.
     const org_id = this.props.organisation._id;
-    console.log('this.state', ': ', this.state);
+    console.log("this.state", ": ", this.state);
 
     const baseURL = process.env.REACT_APP_BASE_URL;
     const url = `${baseURL}/protected/organisation/${org_id}`;
 
     const { data } = this.state;
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const config = {
-      headers:{
+      headers: {
         token
       }
-    }
+    };
 
-    axios.put(url, data, config)
-      .then((resp => {
-        console.log('resp.data', ': ', resp.data);
+    axios
+      .put(url, data, config)
+      .then(resp => {
+        console.log("resp.data", ": ", resp.data);
         updateOrganisation(resp.data);
 
         //Changing to edit mode:
         this.setState({
           formClass: "readMode",
           editButton: "editButton"
-        })
-      }))
-      .catch(err => {
-
+        });
       })
-  }
+      .catch(err => {});
+  };
 
   createTextInput = (attr, description) => {
     const { organisation } = this.props;
     return (
       <React.Fragment>
         <label htmlFor={`${attr}`}> {description} </label>
-        <input type="text" id={`${attr}`} onChange={this.handleInputChange} placeholder={organisation[attr]} />
-        <br></br>
+        <input
+          type="text"
+          id={`${attr}`}
+          onChange={this.handleInputChange}
+          placeholder={organisation[attr]}
+        />
+        <br />
       </React.Fragment>
     );
-  }
+  };
 
-  convertToYesOrNo = (val) => {
+  convertToYesOrNo = val => {
     if (val === true) {
-      return "YES"
+      return "YES";
     } else {
-      return "NO"
+      return "NO";
     }
-  }
+  };
 
   createOptionInput = (attr, description) => {
     const { organisation } = this.props;
@@ -76,65 +80,78 @@ class Organisation extends Component {
       <React.Fragment>
         <label htmlFor={`${attr}`}>{description} </label>
         <select id={`${attr}`} onChange={this.handleInputChange}>
-          <option value="" selected disabled hidden>{this.convertToYesOrNo(organisation[attr])}</option>
+          <option value="" selected disabled hidden>
+            {this.convertToYesOrNo(organisation[attr])}
+          </option>
           <option value="true">YES</option>
           <option value="false">NO</option>
         </select>
-        <br></br>
+        <br />
       </React.Fragment>
     );
-  }
+  };
 
-  edit = (e) => {
+  edit = e => {
     e.preventDefault();
     if (e.target.innerHTML === "Edit") {
-      e.target.innerHTML = "Cancel"
+      e.target.innerHTML = "Cancel";
       this.setState({
         formClass: "editMode",
         editButton: "cancelButton"
-      })
+      });
     } else {
-      e.target.innerHTML = "Edit"
+      e.target.innerHTML = "Edit";
       this.setState({
         formClass: "readMode",
         editButton: "editButton"
-      })
+      });
     }
-  }
+  };
 
   render() {
-
     // Organisation component inherits props from Organisation model, renders object's key/values as an array to Dashboard
     const { organisation } = this.props;
 
     return (
       <React.Fragment>
         <p>Last updated: {organisation.lastUpdated}</p>
-        <p>Organisation: <strong>{organisation.name} </strong></p>
+        <p>
+          Organisation: <strong>{organisation.name} </strong>
+        </p>
 
-        <button onClick={this.edit} className={this.state.editButton}>Edit</button>
+        <button onClick={this.edit} className={this.state.editButton}>
+          Edit
+        </button>
 
         <form id="form" className={this.state.formClass}>
           <button onClick={this.submitForm}>Update</button>
-          <br></br>
+          <br />
           {this.createTextInput("description", "Description:")}
           {this.createTextInput("website", "Website:")}
           {this.createTextInput("abn", "ABN:")}
           {this.createTextInput("providerType", "Provider Type:")}
           {this.createTextInput("alsoKnownAs", "Also Known As:")}
           {this.createTextInput("emailAddress", "Email Address:")}
-          {this.createOptionInput("emailIsConfidential", "Email Is Confidential:")}
+          {this.createOptionInput(
+            "emailIsConfidential",
+            "Email Is Confidential:"
+          )}
           {this.createTextInput("postalAddress", "Postal Address:")}
           {this.createTextInput("postalAddressState", "State:")}
           {this.createTextInput("postalAddressSuburb", "Suburb:")}
           {this.createTextInput("postalAddressPostcode", "Postcode:")}
-          {this.createOptionInput("postalAddressIsConfidential", "Postal Address Is Confidential:")}
+          {this.createOptionInput(
+            "postalAddressIsConfidential",
+            "Postal Address Is Confidential:"
+          )}
           {this.createTextInput("phoneNumber", "Phone Number:")}
           {this.createTextInput("phoneKind", "Phone Kind:")}
-          {this.createOptionInput("phoneIsConfidential", "Phone Is Confidential:")}
+          {this.createOptionInput(
+            "phoneIsConfidential",
+            "Phone Is Confidential:"
+          )}
           {this.createTextInput("ceo", "CEO:")}
         </form>
-
       </React.Fragment>
     );
   }
